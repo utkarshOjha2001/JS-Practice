bf8c31616208d5f2e9225de5c9770ff6235a842a
utkarshOjha2001 <162027256+utkarshOjha2001@users.noreply.github.com>
2024-06-27 08:12:54 +0000

    Container config

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-console.log("Utkarsh");
-alert("utkarsh")
+class Mynumber{
+ 
+    constructor(fnum, snum){
+           this.fnum = fnum
+           this.snum = snum
+    }
+    addNumbers(){
+        return this.fnum + this.snum
+    }
+}
+
+let newNum = new Mynumber(23, 34)
+
+console.log(newNum.addNumbers());
